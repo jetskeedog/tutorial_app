9ca1d94570b8fbd03c56b18a765b25491d49af9c
jetskee96 <jetskee96@yahoo.com>
2015-03-29 20:57:09 +0000

    User settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
                      format: { with: VALID_EMAIL_REGEX },
                      uniqueness: { case_sensitive: false }
    has_secure_password
-   validates :password, length: { minimum: 6 }
+     validates :password, length: { minimum: 6 }, allow_blank: true
+
   
     # Returns the hash digest of the given string.
   def User.digest(string)
@@ -24,10 +25,13 @@ class User < ActiveRecord::Base
     return false if remember_digest.nil?
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
   
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
